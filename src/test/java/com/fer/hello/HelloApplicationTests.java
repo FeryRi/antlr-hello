c87a0e89d050d594a117e88a4190d2a008e99907
FeryRi <A01656047@tec.mx>
2025-11-07 17:25:34 -0600

    add antlr-hello

---
+++ src/test/java/com/fer/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
